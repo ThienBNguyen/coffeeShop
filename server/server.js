eb53847cf4224776ece5766513b3599e19f2ea74
ThienBNguyen <thien.nguyen30191@gmail.com>
2022-05-03 10:17:40 -0500

    deploy deroku

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ const express = require('express');
 //parse incoming request bodies in a middeware before your handler, available under the req.body** enable to sent body request
 const bodyParser = require('body-parser');
 // mongoose allow connect to the database create model for database
+// const https = require('https');
+// const fs = require('fs');
 const mongoose = require('mongoose');
-
 // For info routes
 const infoRoutes = require('./routers/info-routes');
 
@@ -16,20 +17,13 @@ const menuRoutes = require('./routers/menu-routes');
 const userRoutes = require('./routers/user-routes');
 // import HttpError model
 const HttpError = require('./models/http-error');
+const cors = require('cors');
 
 const app = express();
-
-// For bodyParser
-app.use(bodyParser.json());
-
 // set CORS headers avoid CORS error
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
-	next();
-});
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // use routes
 app.use('/info', infoRoutes);
@@ -49,28 +43,32 @@ app.use((error, req, res, next) => {
 	res.status(error.code || 500);
 	res.json({ message: error.message || 'an unknown error occurred!' });
 });
-// Listen to our 5000 port
-let port = process.env.PORT;
-if (port == null || port == '') {
-	port = port;
-}
 // Connect to database
-// Get username and password from .env file
-const dbPass = process.env.dbPass;
-const dbName = process.env.dbName;
-const dbConnect = `mongodb+srv://${dbName}:${dbPass}@cluster0.yrynw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const dbConnect = `${process.env.MONGODB_CONNECTION_STRING}`;
 mongoose
 	.connect(dbConnect, {
 		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false
+		useUnifiedTopology: true
 	})
 	.then(() => {
-		app.listen(port, function(res) {
-			console.log('server running on port 5000');
-		});
+		console.log(`MongoDB has been connected`);
 	})
 	.catch((err) => {
 		console.log(err);
 	});
+
+const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+	// Step 1:
+	app.use(express.static('client/build'));
+	// Step 2:
+	const path = require('path');
+
+	app.get('*', function(req, res) {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log(`server running on PORT ${PORT}`);
+});
